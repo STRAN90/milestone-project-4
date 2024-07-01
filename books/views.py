8908d77e274c782efbc88aafe0fe89e03b1eec46
Sonia Majewska <sonianltran@gmail.com>
2024-07-01 14:39:45 +0000

    Add product details functionality and correct typos

--- books/views.py
+++ books/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
-# Create your views here.
 
 def all_books(request):
     """ A view to show all books, including sorting and searching queries """
-
     books = Book.objects.all()
 
     context = {
         'books': books,
     }
 
-    return render(request, 'books/books.html', context) 
+    return render(request, 'books/books.html', context)
+
+
+def book_detail(request, book_id):
+    """ A view to show individual book details """
+    book = get_object_or_404(Book, pk=book_id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
+
