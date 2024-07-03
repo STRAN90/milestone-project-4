9615e7902fde0a4d3c1ca4ed501a36c44a2e56fd
Sonia Majewska <sonianltran@gmail.com>
2024-07-03 09:16:17 +0000

    Add notification css, shopping bag preview and messages

--- bag/views.py
+++ bag/views.py
@@ -6,12 +6,10 @@ from django.contrib import messages
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
     book = get_object_or_404(Book, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -47,15 +45,19 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
-    
-    book = get_object_or_404(Book, pk=item_id)
-    bag = request.session.get('bag', {})
+    try:
+        book = get_object_or_404(Book, pk=item_id)
+        bag = request.session.get('bag', {})
 
-    if item_id in bag:
-        bag.pop(item_id)
-        messages.success(request, f'Removed {book.title} from your shopping bag')
-    else:
-        messages.error(request, f'Item {book.title} not found in your shopping bag')
+        if item_id in bag:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {book.title} from your shopping bag')
+        else:
+            messages.error(request, f'Item {book.title} not found in your shopping bag')
 
-    request.session['bag'] = bag
-    return HttpResponse(status=200)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
