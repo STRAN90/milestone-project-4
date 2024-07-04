1a655cd48413092ae3157f528f30d785e1df95c4
Sonia Majewska <sonianltran@gmail.com>
2024-07-04 15:32:49 +0000

    Add stripe elememts

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PYrDIRtIY6RM9I4j5OjQbO7YEEf3IgJxGG9HFiGzZPYhxsyyB0abTIuCwnszYCOvfeaDtoUQklXaUuCbHpQvcyn00iR2pixNW',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
