42bcf07929380b19db4a4f25c4b61a69130c9041
Sonia Majewska <sonianltran@gmail.com>
2024-07-13 14:44:31 +0000

    Remove secret key and set debug

--- cookbooknook/settings.py
+++ cookbooknook/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_6h-x@1h(4$d8b!j1$vq%@savep6t9c)62u-!w6^gtqqfrj9e4'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cookbooknook-f2a539e614c9.herokuapp.com', '8000-stran90-milestoneprojec-hkhq0i5kxqa.ws.codeinstitute-ide.net', 'localhost']
 
