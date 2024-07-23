697cd46c7f2b9341d6816b9ea4d44b684eea1981
Sonia Majewska <sonianltran@gmail.com>
2024-07-23 20:06:08 +0000

    Add static and media link to settings.py

--- cookbooknook/settings.py
+++ cookbooknook/settings.py
@@ -196,11 +196,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://cookbooknook1.s3.amazonaws.com/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://cookbooknook1.s3.amazonaws.com/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
